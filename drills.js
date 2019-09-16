2c14bad66a55b6c45b30c26e318286fe9b19fc53
Alex Bannow <rbannal@gmail.com>
2019-09-16 14:36:16 -0400

    added year of birth

--- drills.js
+++ drills.js
@@ -1,8 +1,9 @@
 'use strict';
 
 function createGreeting(name, age) {
-  return `Hi, my name is ${name} and I'm ${age} years old`;
+    const yearOfBirth = 2019 - age;
+  return `Hi, my name is ${name} and I'm ${age} years old.  I was born in ${yearOfBirth}.`;
 }
 
-const greeting1 = createGreeting();
+const greeting1 = createGreeting(Bob, 57);
 console.log(greeting1);
