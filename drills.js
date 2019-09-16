ae9a8f4735a93f2a89fd95b9e79757607f080274
Michael Kirsch <michaelskirsch@gmail.com>
2019-09-16 12:53:12 -0600

    added error checking for negative ages

--- drills.js
+++ drills.js
@@ -1,12 +1,18 @@
 'use strict';
 function getYearOfBirth(age) {
-    const yearOfBirth = 2019 - age;
-    return yearOfBirth;
+  if (age < 0){
+    throw new Error('Age can not be negative');
+  }
+  const yearOfBirth = 2019 - age;
+  return yearOfBirth;
 }
 function createGreeting(name, age) {
-    const yob = getYearOfBirth(age);
+  const yob = getYearOfBirth(age);
   return `Hi, my name is ${name} and I'm ${age} years old.  I was born in ${yob}.`;
 }
-
-const greeting1 = createGreeting('Bob', 57);
-console.log(greeting1);
+try {
+  const greeting1 = createGreeting('Bob', 57);
+  console.log(greeting1);
+} catch(e) {
+  console.log(e);
+}
