2c0b4510a5909cf2330fa841f27dc48a839e8e34
Michael Kirsch <michaelskirsch@gmail.com>
2019-09-16 12:32:19 -0600

    createGreeting now takes parameters

--- drills.js
+++ drills.js
@@ -1,5 +1,7 @@
-function createGreeting() {
-    return 'Hi, my name is Alex and I\'m 33 years old';
+'use strict';
+
+function createGreeting(name, age) {
+  return `Hi, my name is ${name} and I'm ${age} years old`;
 }
 
 const greeting1 = createGreeting();
